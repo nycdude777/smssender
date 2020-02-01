e8a07d84fa5d853f9fd46fe57a2323ad46bd1605
Rick Ellis <riccello@yahoo.com>
2020-01-31 21:26:08 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const {MongoClient} = require('mongodb');
 
-const dbHost = 'localhost:27017';
+const url = 'mongodb://localhost:27017';
 const dbName = 'optin';
 
 async function listDatabases(client){
     databasesList = await client.db().admin().listDatabases();
- 
     console.log("Databases:");
     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
 };
@@ -15,22 +14,30 @@ async function main(){
      * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
      * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
      */
-    const uri = "mongodb+srv://" + dbHost + "/test?retryWrites=true&w=majority";
+    
+    const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
  
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
- 
-    try {
-        // Connect to the MongoDB cluster
-        await client.connect();
- 
-        // Make the appropriate DB calls
-        await  listDatabases(client);
- 
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
+    // Database Name
+    const dbName = 'myproject';
+
+    // Create a new MongoClient
+    const client = new MongoClient(url);
+
+    // Use connect method to connect to the Server
+    client.connect(function(err) {
+        assert.equal(null, err);
+
+        console.log("Connected successfully to server");
+
+        const db = client.db(dbName);
+
+        console.log("Connected successfully to " + dbName);
+
+        listDatabases(client);
+
+        client.close();
+    });
+
 }
 
 main().catch(console.error);
