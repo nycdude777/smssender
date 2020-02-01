fe4ae5b19503b58dbfdfd0d9d88391a0d0922f81
Rick Ellis <riccello@yahoo.com>
2020-02-01 13:12:34 -0600

    Syncing changes

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const {MongoClient} = require('mongodb');
 
-const dbHost = 'localhost:27017';
+const url = 'mongodb://localhost:27017';
 const dbName = 'optin';
 
 async function listDatabases(client){
-    databasesList = await client.db().admin().listDatabases();
- 
     console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+    
+    databasesList = await client.db().admin().listDatabases();
+    
+    console.log('databaseList: ');
+    console.log(databasesList);
+    
+    console.log('Enumerating db list: ');
+    
+    for(let db of databasesList.databases) {
+        console.log(` - ${db.name}`);
+    }
+    
 };
 
 async function main(){
@@ -15,22 +24,40 @@ async function main(){
      * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
      * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
      */
-    const uri = "mongodb+srv://" + dbHost + "/test?retryWrites=true&w=majority";
- 
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
- 
-    try {
-        // Connect to the MongoDB cluster
-        await client.connect();
- 
-        // Make the appropriate DB calls
-        await  listDatabases(client);
- 
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
+    
+    console.log("Begin mongo db connection test");
+    
+    // Create a new MongoClient
+    const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+     
+    console.log("MongoClient is " + typeof client);
+    
+    // Use connect method to connect to the Server
+    client.connect(function(err) {
+    
+        if(err) {
+            console.log(err);
+            console.log("Exiting");
+            return;
+        }
+
+        console.log("Connected successfully to server");
+
+        listDatabases(client);
+        
+        client.close();
+        
+        return;
+        
+        const db = client.db(dbName);
+
+        console.log("Connected successfully to " + dbName);
+
+        
+
+        
+    });
+
 }
 
 main().catch(console.error);
